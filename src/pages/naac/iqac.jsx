817046f501b848649cff8815f1f756cd6f1df348
Aakanksha <akankshathalkar@gmail.com>
2026-09-09 09:57:33 +0530

    iqac page added in naac folder

---
+++ src/pages/naac/iqac.jsx
@@ -0,0 +1,21 @@
+const Iqac = () => {
+  return (
+    <main className="min-h-screen bg-gray-50 py-16 px-6">
+
+      <div className="max-w-6xl mx-auto">
+
+        <h1 className="text-4xl font-bold text-[#0b2d4d]">
+          IQAC
+        </h1>
+
+        <p className="mt-4 text-gray-600">
+          Welcome to PIRENS IBMA Iqac Page.
+        </p>
+
+      </div>
+
+    </main>
+  );
+};
+
+export default Iqac;
